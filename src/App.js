9229e039dce02cd66a5bd2d4ead2c0ec97711132
hassen <hasansaid51@gmail.com>
2019-12-21 20:27:55 -0800

    api call made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      await (await (await fetch('https://api.github.com/users'))).json()
+      .then(data => setUsers(data))
+    }
+    fetchData()
+  }, [])
+
+  const all = users.map((elem) => {
+    return(
+      <div>
+    <div key={elem.id}>{elem.login}</div>
+    <img src={elem.avatar_url} alt="github images"/>
+    </div>
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(users)}
+      Hello world!
+      {all}
     </div>
   );
 }
