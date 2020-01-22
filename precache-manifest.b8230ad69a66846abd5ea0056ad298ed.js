45049a7c3571b6fddfaae00afad913b860187bd5
HassenH1 <hasansaid51@gmail.com>
2020-01-21 19:47:35 -0800

    Updates

---
+++ precache-manifest.b8230ad69a66846abd5ea0056ad298ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a844268839a7779ce056917315e634",
+    "url": "/Githubfinder/index.html"
+  },
+  {
+    "revision": "ae1512788b8587ff010b",
+    "url": "/Githubfinder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "32388244bd90493c5cca",
+    "url": "/Githubfinder/static/js/2.55a86488.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Githubfinder/static/js/2.55a86488.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae1512788b8587ff010b",
+    "url": "/Githubfinder/static/js/main.08f6d811.chunk.js"
+  },
+  {
+    "revision": "b78848cfb0c68e3bb001",
+    "url": "/Githubfinder/static/js/runtime-main.37383e34.js"
+  }
+]);
